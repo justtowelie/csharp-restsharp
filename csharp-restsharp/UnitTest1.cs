ff767225b0775fab2d3baae98a07734d0671f327
Daniel Leggett <danielleggett1@gmail.com>
2024-03-18 16:38:24 +0000

    Add project files.

---
+++ csharp-restsharp/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace csharp_restsharp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
